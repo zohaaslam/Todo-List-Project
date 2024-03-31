6d81d082aaee661894b65379d230d26eff744ee8
Zoha Aslam <160209815+zohaaslam@users.noreply.github.com>
2024-04-01 03:20:07 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let todos = [];
+let condition = true;
+while (condition) {
+    let addTask = await inquirer.prompt([
+        {
+            name: "todos",
+            message: "what u want to do?",
+            type: "input"
+        },
+        {
+            name: "addMore",
+            message: "Do you wan to add more?",
+            type: "confirm",
+            default: "false"
+        }
+    ]);
+    todos.push(addTask.todos);
+    condition = addTask.addMore;
+    console.log(todos);
+}
